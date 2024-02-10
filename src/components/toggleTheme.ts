5343e303527c5bd7d1b29eff194fad27518dbbf7
Daniel Ives <danielives012@yahoo.com>
2024-02-10 16:09:21 -0700

    fix: remove console logs

--- src/components/toggleTheme.ts
+++ src/components/toggleTheme.ts
@@ -4,7 +4,6 @@ export type Theme = 'light' | 'dark' | 'system';
 export function setTheme(theme: Theme) {
     if (theme === "system") {
         const isDarkSystemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        console.log("theme is system", isDarkSystemTheme)
 
         if (isDarkSystemTheme) {
             document.documentElement.classList.add("dark");
@@ -12,7 +11,6 @@ export function setTheme(theme: Theme) {
             document.documentElement.classList.remove("dark");
         }
     } else {
-        console.log("theme is not system", theme)
         if (theme === "dark") {
             document.documentElement.classList.add("dark");
         } else {
