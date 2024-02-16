887c217f0fa49eaa0e20e4c9d486a620354469bf
Daniel Ives <danielives012@yahoo.com>
2024-02-15 22:08:26 -0700

    feat: prefetch?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true
+  },
   integrations: [react(), tailwind({
     applyBaseStyles: false
   }), icon()]
