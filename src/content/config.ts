1fee5be6ee636d35ac976579e96d91106b66bd59
Daniel Ives <danielives01@gmail.com>
2024-02-17 22:03:23 -0700

    wip: my work page

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+// 2. Define your collection(s)
+const projectCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        description: z.string(),
+        githubLink: z.string(),
+        demoLink: z.string(),
+        youtubeLink: z.string(),
+    })
+
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+    'projects': projectCollection
+};
---
+++ src/content/projects/CoffeeCalculator.md
@@ -0,0 +1,11 @@
+---
+title: Coffee Calculator
+date: Jan 2022
+description: A simple frontend that details various coffee brewing methods, built with Next.js (v12) and chakra-ui
+githubLink: https://github.com/daives01/coffee-calculator
+demoLink: https://coffee-calculator.daniel-ives.com
+youtubeLink: '' 
+
+---
+
+hi mom
