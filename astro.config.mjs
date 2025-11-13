1d8e74f322009095de6dae0fbdf525a31a6ce285
Daniel Ives <danielives012@yahoo.com>
2025-11-12 20:13:21 -0700

    Complete minimal redesign: remove React, simplify layout, add Nunito font, system theme support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,17 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    prefetchAll: true,
-  },
   redirects: {
     '/tesla/daniel': 'https://ts.la/daniel395842',
     '/tesla/quincy': 'https://ts.la/quincy11390',
     '/tesla': 'https://ts.la/quincy11390'
-    },
+  },
   integrations: [
-    react(),
     tailwind({
       applyBaseStyles: false,
     }),
-    icon(),
   ],
 });
 
