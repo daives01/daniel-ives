7a4fcf892907c0422486e8fcf1a77b6c75679e04
daives01 <danielives012@yahoo.com>
2025-12-30 21:54:04 -0700

    feat: add posts and first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  integrations: [mdx()],
   redirects: {
     '/tesla/daniel': 'https://ts.la/daniel395842',
     '/tesla/quincy': 'https://ts.la/quincy11390',
--- src/content/config.ts
+++ src/content/config.ts
@@ -16,8 +16,18 @@ const projectCollection = defineCollection({
     })
 
 });
+
+const postCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        draft: z.boolean().optional().default(false)
+    })
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-    'projects': projectCollection
+    'projects': projectCollection,
+    'posts': postCollection
 };
